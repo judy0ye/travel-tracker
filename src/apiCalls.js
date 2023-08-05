848836691cce3dd99de1f2e48d91709b7ba1dbab
Judy Ye <judyy.ye@gmail.com>
2023-08-04 21:15:40 -0700

    Add window event listener to receive endpoint data on console

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,20 @@
-const fetchFromEndPoints = travelInfo => {
-  fetch(`http://localhost:3001/api/v1/${travelInfo}`)
+// --------- Endpoint URLs
+const allEndpointURLs = [
+  'http://localhost:3001/api/v1/travelers', 
+  'http://localhost:3001/api/v1/trips',
+  'http://localhost:3001/api/v1/destinations'
+]
+
+// --------- GET request
+const fetchFromEndPoints = () => {
+  return allEndpointURLs.map(endpointURL => 
+    fetch(endpointURL)
     .then(res => res.json())
+    )
+    
 }
 
-export{
+export {
   fetchFromEndPoints
 }
+
--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,8 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
+import {
+  fetchFromEndPoints
+} from './apiCalls'
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
@@ -9,3 +12,14 @@ import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+window.addEventListener('load', function () {
+  Promise.all(fetchFromEndPoints())
+    .then(data => {
+      console.log('data:', data)
+      console.log('travelers: ', data[0].travelers);
+      console.log('trips: ', data[1].trips)
+      console.log('destinations: ', data[2].destinations)
+    })
+})
+
